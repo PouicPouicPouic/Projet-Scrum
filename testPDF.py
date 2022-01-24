fcf4b5364f7b519f3e8ad636531da5ebf0f9efa0
aaribard <37927092+aaribard@users.noreply.github.com>
2022-01-24 17:28:29 +0100

    Add files via upload

---
+++ testPDF.py
@@ -0,0 +1,19 @@
+import pdftotext
+
+# Load your PDF
+with open("Das_Martins.pdf", "rb") as f:
+    pdf = pdftotext.PDF(f)
+
+# How many pages?
+print(len(pdf))
+
+# Iterate over all the pages
+for page in pdf:
+    print(page)
+
+# Read some individual pages
+print(pdf[0])
+print(pdf[1])
+
+# Read all the text into one string
+print("\n\n".join(pdf))
